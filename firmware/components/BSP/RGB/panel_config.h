5c27202b3b462b6d21ec27a8475be4cd7b0f653b
AndyXie_surface <andyxie0718@outlook.com>
2026-03-15 14:59:36 +0800

    Refactor panel configuration to enable column-major wiring

--- firmware/components/BSP/RGB/panel_config.h
+++ firmware/components/BSP/RGB/panel_config.h
@@ -5,18 +5,24 @@
 #define PANEL_HEIGHT 16
 #define PANEL_LED_COUNT (PANEL_WIDTH * PANEL_HEIGHT)
 
-// 1: odd rows reversed, 0: normal row-major.
-#define PANEL_SERPENTINE 1
+// Column-major straight wiring: each column advances in +y order.
+#define PANEL_COLUMN_MAJOR 1
+#define PANEL_SERPENTINE 0  // 1: odd rows reversed, 0: normal row-major.
+
 
 // Unified max value for LED channels used by all simulators.
 #define PANEL_LED_VALUE_MAX 20
 
 static inline int panel_led_index(int x, int y) {
-#if PANEL_SERPENTINE
-    if (y & 1) {
-        return y * PANEL_WIDTH + (PANEL_WIDTH - 1 - x);
-    }
+#if PANEL_COLUMN_MAJOR
+    return x * PANEL_HEIGHT + y;
 #endif
+
+#if PANEL_SERPENTINE
+    if (y & 1) 
+        return y * PANEL_WIDTH + (PANEL_WIDTH - 1 - x);
+#endif
+
     return y * PANEL_WIDTH + x;
 }
 
